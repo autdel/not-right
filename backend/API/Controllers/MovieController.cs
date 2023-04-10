141157764badbc79a41df5d56e64435315a1873b
autdel <114094641+autdel@users.noreply.github.com>
2023-04-10 04:20:47 -0600

    Add files via upload

---
+++ backend/API/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+
+        public MovieController(MovieDbContext context)
+        {
+            this.context = context;
+        }
+
+        // Gets the movie information from the database
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies.Where(m => m.Edited == "Yes").OrderBy(m => m.Title).ToArray();
+        }
+    }
+}
